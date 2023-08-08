f06140ec90ff5d666e82340b146b6197591116bb
Heri Priyatno <duakilo.net@gmail.com>
2023-08-08 14:36:50 +0700

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMzQ1NQ/spectral.js?branch_slug=master&token=c8cb16d0-e1a7-4c25-b05e-9bc98ca136b1";
+export default { extends: ruleset };
